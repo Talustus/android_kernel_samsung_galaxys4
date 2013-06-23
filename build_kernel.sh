bef4a6bc05db3b1d649cbfa37c8862c6ea0c0a9e
Talustus <talustus@dream-irc.de>
2013-06-23 10:59:38 +0200

    DEFCONFIG: disable SEC_DVFS, enable user Voltage table

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,7 +17,7 @@ TXTCLR='\e[0m'    		# Text Reset
 ## Version of this Build
 #
 ## 1.0 for initial build
-KRNRLS="DreamKernel-GT-I9505-v1.0.2-CM10.1"
+KRNRLS="DreamKernel-GT-I9505-v1.0.2-CM10.1_VDD"
 
 #
 ## Create TAR File for ODIN?
