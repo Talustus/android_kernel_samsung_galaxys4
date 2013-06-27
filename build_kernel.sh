acf42b2de56b5f0eba542c5d5c86000c1e3f93b6
Talustus <talustus@dream-irc.de>
2013-06-27 08:43:59 +0200

    Defconfig/buildscript: bump version for no reason :D

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,7 +17,7 @@ TXTCLR='\e[0m'    		# Text Reset
 ## Version of this Build
 #
 ## 1.0 for initial build
-KRNRLS="DreamKernel-GT-I9505-v1.0.2-CM10.1_VDD-OC2"
+KRNRLS="DreamKernel-GT-I9505-v1.0.4-CM10.1"
 
 #
 ## Create TAR File for ODIN?
